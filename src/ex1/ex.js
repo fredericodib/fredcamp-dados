50802aa7405574c96dd2a45b46602af89a21e627
fredericodib <fredericopdib@gmail.comclear>
2021-05-30 19:06:08 -0300

    ex1

--- src/ex1/ex.js
+++ src/ex1/ex.js
@@ -1,7 +1,13 @@
 // retorne a media dos elementos dentroo do  vetor
 // dica: use forEach
 function ex(entrada) {
-  return entrada;
+  let media;
+  let soma = 0;
+  for (i = 0; i < entrada.length; i++) {
+    soma += entrada[i];
+  }
+  media = soma / entrada.length;
+  return media;
 }
 
 module.exports = ex;
