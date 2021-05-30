0622e9a0e00d1f4cf0d9bb7dc26ab4626bf21ef5
fredericodib <fredericopdib@gmail.comclear>
2021-05-30 18:53:45 -0300

    ex 9

---
+++ src/ex9/ex.js
@@ -0,0 +1,8 @@
+// Receba um array comm varios objetos
+// Cada objeto possui duas chaves,  nome e idade
+// Retorne um array com todas as idades ordenadas
+function ex(entrada) {
+  return entrada;
+}
+
+module.exports = ex;
---
+++ src/ex9/saida1.js
@@ -0,0 +1,3 @@
+const saida = [24, 32, 43, 65, 224];
+
+module.exports = saida;
