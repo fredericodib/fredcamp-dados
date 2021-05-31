ea6d3a053b61acc41e27f880d3df09a5b41d656a
Clara Mendes <claraplanejamento@gmail.com>
2021-05-31 12:18:48 -0300

    :sparkles: adds ex 5

--- src/ex5/ex.js
+++ src/ex5/ex.js
@@ -1,8 +1,13 @@
-// Receba um objeto que as chaves são o nome das pessoas e o valor á a idade
+// Receba um objeto que as chaves são o nome das pessoas e o valor é a idade
 // Devolva um array com o nome das pessas
 // Dica: pequise por Object keys, values, entries
 function ex(entrada) {
-  return entrada;
+  let saida = [];
+  for (const key in entrada){
+    saida.push(String(key))
+  }
+
+  return saida;
 }
 
 module.exports = ex;
