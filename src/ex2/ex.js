b2974636716484bff290b286f095bae8d5fc49e4
Clara Mendes <claraplanejamento@gmail.com>
2021-05-31 10:21:42 -0300

    :sparkles: adds ex 2

--- src/ex2/ex.js
+++ src/ex2/ex.js
@@ -1,7 +1,8 @@
-// retorne um vetor com cada elemento da entrada ao  quadrado
+// retorne um vetor com cada elemento da entrada ao quadrado
 // dica: use map
 function ex(entrada) {
-  return entrada;
+  let saida = entrada.map(element => element**2)
+  return saida;
 }
 
 module.exports = ex;
