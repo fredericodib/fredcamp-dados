4e72ddcd7ec15459033b0dfa7570cef8c97df201
fredericodib <fredericopdib@gmail.comclear>
2021-05-31 23:11:00 -0300

    ex 17

---
+++ src/ex17/entrada1.js
@@ -0,0 +1,40 @@
+const entrada = [
+  {
+    nome: "fred",
+    idade: 33,
+    tamanho: 34,
+    id: "j4j4ffsf",
+  },
+  {
+    nome: "Clarita",
+    idade: 22,
+    tamanho: 340,
+    id: "aqaqaqaq",
+  },
+  {
+    nome: "Pedrinha",
+    idade: 92753,
+    tamanho: 14,
+    id: "uyuy3232",
+  },
+  {
+    nome: "Bambuzinha",
+    idade: 11,
+    tamanho: 4,
+    id: "qquy3232",
+  },
+  {
+    nome: "Paixao",
+    idade: 33,
+    tamanho: 12,
+    id: "sdfsdfsf",
+  },
+  {
+    nome: "Giu",
+    idade: 33,
+    tamanho: 132,
+    id: "erwrwet",
+  },
+];
+
+module.exports = entrada;
---
+++ src/ex17/ex.js
@@ -0,0 +1,10 @@
+// Voce recebera um array com varios usuários.
+// Ordene os usuários de acordo com a idade
+// caso a idade for iqual deve ser comparado o tamanho da pessoa
+// Dica: Parecido com a questão anterior, mas vc vai precisar colocar
+// um pouco mais de lógica na função de ordenar, talvez adicionar uns if
+function ex(entrada) {
+  return entrada;
+}
+
+module.exports = ex;
