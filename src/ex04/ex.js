2c07d96a7341a9084e5779194aaa1789254366d0
Clara Mendes <claraplanejamento@gmail.com>
2021-06-01 21:31:13 -0300

    :recycle: refactors ex 04

--- src/ex04/ex.js
+++ src/ex04/ex.js
@@ -2,15 +2,36 @@
 // Não pode ordenar o array
 function ex(entrada) {
   let saida;
-  entrada.forEach(element => {
-    if (entrada.lastIndexOf(element) !== -1) {
-      if (entrada.lastIndexOf(element) !== entrada.indexOf(element)){
-        saida = element
-      }
+  let repet = {};
+  entrada.forEach((element) => {
+    if (!repet[element]) {
+      repet[element] = 1;
+    } else {
+      repet[element] += 1;
     }
   });
+
+  for (key in repet) {
+    if (repet[key] !== 1) {
+      saida = Number(key);
+    }
+  }
 
   return saida;
 }
 
 module.exports = ex;
+
+// SOLUÇÃO ANTIGA
+// function ex(entrada) {
+//   let saida;
+//   entrada.forEach(element => {
+//     if (entrada.lastIndexOf(element) !== -1) {
+//       if (entrada.lastIndexOf(element) !== entrada.indexOf(element)){
+//         saida = element
+//       }
+//     }
+//   });
+
+//   return saida;
+// }
