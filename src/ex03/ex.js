df03b0614e89e2801d0b31da61e0899d545eb1d7
Clara Mendes <claraplanejamento@gmail.com>
2021-06-01 21:03:44 -0300

    :recycle: refactors ex 03

--- src/ex03/ex.js
+++ src/ex03/ex.js
@@ -3,7 +3,7 @@
 function ex(entrada) {
   let ordenada = entrada.sort(function(a, b){return a-b})
   let saida
-  for (i=0; i<ordenada.length; i++) {
+  for (i=0; i<ordenada.length-1; i++) {
     if (ordenada[i+1] === ordenada[i]) {
       saida = ordenada[i]
     }
