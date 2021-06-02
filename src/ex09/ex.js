2ec7036ea107eccb1ce9e993f50e0bf54a41d64b
Clara Mendes <claraplanejamento@gmail.com>
2021-06-01 22:37:56 -0300

    :recycle: refactors ex 09

--- src/ex09/ex.js
+++ src/ex09/ex.js
@@ -2,16 +2,26 @@
 // Cada objeto possui duas chaves, nome e idade
 // Retorne um array com todas as idades ordenadas
 function ex(entrada) {
-  let saida = []
-  let idades = []
+  let saida = [];
 
-  entrada.forEach(element => {
-    idades.push(element.idade)
-  })
+  let idades = entrada.map((element) => element.idade);
 
-  idades.filter(Number)
-  saida = idades.sort(function(a, b){return a-b})
+  saida = idades.sort((a, b) => a - b);
   return saida;
 }
 
 module.exports = ex;
+
+// RESOLUÇÃO ANTIGA
+// function ex(entrada) {
+//   let saida = []
+//   let idades = []
+
+//   entrada.forEach(element => {
+//     idades.push(element.idade)
+//   })
+
+//   idades.filter(Number)
+//   saida = idades.sort(function(a, b){return a-b})
+//   return saida;
+// }
