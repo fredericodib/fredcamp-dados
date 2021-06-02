b666fffbf88f5486f129ce010bc6bb3f5747b97e
Clara Mendes <claraplanejamento@gmail.com>
2021-06-02 00:00:08 -0300

    :recycle: adds ex 10 // ESSA DISGRAÇA QUE EU TENTEI REORDENAR E NEM PRECISAVA

--- src/ex10/ex.js
+++ src/ex10/ex.js
@@ -1,6 +1,15 @@
 // Faça o oposto do exercício 8.
 function ex(entrada) {
-  return entrada;
+
+  let saida= entrada.map(element => {
+    let ob = {}
+    key1 = Object.keys(element)[0]
+    value = Object.values(element)[0] 
+    ob={nome: key1, idade: value}
+     return ob
+   })
+
+  return saida;
 }
 
 module.exports = ex;
