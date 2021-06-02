993ad796aab910c0947daadb7a35310f3c2428bf
Clara Mendes <claraplanejamento@gmail.com>
2021-06-02 11:48:09 -0300

    :recycle: rerefactors ex 07

--- src/ex07/ex.js
+++ src/ex07/ex.js
@@ -2,16 +2,12 @@
 // Devolva um array com varios objetos, cada um sendo uma pessoa
 // Dica: pequise por Object keys, values, entries
 function ex(entrada) {
-  let saida = [];
-  for (var [key, value] of Object.entries(entrada)) {
-    saida.push({[key]: value})
-}
+  const saida = Object.entries(entrada).map((e) => ({ [e[0]]: e[1] }));
 
   return saida;
 }
 
 module.exports = ex;
-
 
 // RESOLUÇÃO ANTIGA
 // function ex(entrada) {
@@ -19,6 +15,16 @@ module.exports = ex;
 //   for (const key in entrada){
 //     saida.push({[key]: entrada[key]})
 //   }
+
+//   return saida;
+// }
+
+// SOLUÇÃO ANTIGA 2
+// function ex(entrada) {
+//   let saida = [];
+//   for (var [key, value] of Object.entries(entrada)) {
+//     saida.push({[key]: value})
+// }
 
 //   return saida;
 // }
