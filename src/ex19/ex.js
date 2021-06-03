2abe4e7411f3a9fe9084ec2e66c6952f4a045167
Clara Mendes <claraplanejamento@gmail.com>
2021-06-03 12:39:12 -0300

    :sparkles: adds ex 19

--- src/ex19/ex.js
+++ src/ex19/ex.js
@@ -9,7 +9,21 @@
 // todos os quartos.
 // depois ordene os usuários de acordo com a variável criada
 function ex(entrada) {
-  return entrada;
+  entrada.forEach(user => {
+    const quarto = Object.values(user.quartos)
+    let soma = 0
+  
+    for (i in quarto){
+      soma += quarto[i].valor
+    }
+  
+    user.totalPrice = soma
+  })
+
+  const saida = entrada
+    .sort((a, b) => a.totalPrice - b.totalPrice)
+
+  return saida;
 }
 
 module.exports = ex;
