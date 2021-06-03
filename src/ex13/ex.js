e5e65aa4e948562c4b4b5b472bb00597b83c13eb
Clara Mendes <claraplanejamento@gmail.com>
2021-06-02 23:33:50 -0300

    :sparkles: adds ex 13

--- src/ex13/ex.js
+++ src/ex13/ex.js
@@ -3,6 +3,10 @@
 // Adicione a key do usuário dentro do objeto do usuário com o nome id.
 // dica: seria bom usar o "const key in entrada"
 function ex(entrada) {
+  for (key in entrada) {
+    entrada[key].id = String(key)
+    }
+
   return entrada;
 }
 
